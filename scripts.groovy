8f636f9615ee2ff9beb4ed2b79c1496d0de42f37
MajbriOussema <oussemamajbri@insat.u-carthage.tn>
2022-12-08 22:19:47 +0100

    updated JenkinsFILE

--- scripts.groovy
+++ scripts.groovy
@@ -1,5 +1,8 @@
 def git_clone(){
     git "https://github.com/MajbriOussema/DevOpsProject"
 }
+def sonarqube_scan(){
+    sh "mvn sonar:sonar -Dsonar.login=admin -Dsonar.password=oussema"
 
+}
 return this
