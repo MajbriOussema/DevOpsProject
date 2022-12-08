8936b81643e4b7ff2b580809536f9a8e1e0f11ab
MajbriOussema <oussemamajbri@insat.u-carthage.tn>
2022-12-09 00:07:54 +0100

    added nexus

--- scripts.groovy
+++ scripts.groovy
@@ -6,7 +6,12 @@ def run_unit_tests(){
     sh "mvn test"
 }
 def sonarqube_scan(){
-    sh "mvn sonar:sonar -Dsonar.login=admin -Dsonar.password=oussema"
-
+    sh "mvn sonar:sonar 
+        -Dsonar.projectKey=devops-project
+        -Dsonar.login=admin 
+        -Dsonar.password=oussema"
+}
+def nexus_deploy(){
+    sh "mv clean deploy"
 }
 return this
