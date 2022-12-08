fedfd349006b70721b491504e1aaf0e1d9ab086c
MajbriOussema <oussemamajbri@insat.u-carthage.tn>
2022-12-09 00:31:49 +0100

    added nexus

--- scripts.groovy
+++ scripts.groovy
@@ -12,6 +12,6 @@ def nexus_deploy(){
     sh "mvn clean package -Dmaven.test.skip=true deploy:deploy-file -DgroupId=com.example -DartifactId=devops-project -Dversion=1.0 -DgeneratePom=true -Dpackaging=jar -DrepositoryId=deploymentRepo -Durl=http://localhost:8081/repository/maven-releases/ -Dfile=target/devops-project-1.0.jar"
 }
 def build_docker_image(){
-    sh "docker build -t devopsproject ."
+    sh "sudo docker build -t devopsproject ."
 }
 return this
